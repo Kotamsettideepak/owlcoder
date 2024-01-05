7be060821ec85689aca903bf50021ca958719a6c
Kotamsettideepak <98525642+Kotamsettideepak@users.noreply.github.com>
2024-01-05 22:13:35 +0530

    LeetCodeJan5.java

--- LeetCodeJan5.java
+++ LeetCodeJan5.java
@@ -1,3 +1,16 @@
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+
+Proceduer:
+    take an array with given array size and fillit with 1's using Array.fill(Array_name,1)
+    now travel on nums check how many numbers are less than the current and parallely check corresponding index which we take all elements to 1
+    in thaht take maximum and add it to the corresponding main number index on which loop is iteerating 'i'th index
+    take another max and check parallely the max arr[i]
+    return max;
+
+    
+    
 import java.util.*;
 import java.lang.Math;
 
