a36120827656b6d83fc09bebad6ebf108e2b357c
Kotamsettideepak <98525642+Kotamsettideepak@users.noreply.github.com>
2024-02-07 22:50:25 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Max Sum without Adjacents/max-sum-without-adjacents.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().findMaxSum(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public int dp[];
+
+    public int moon(int arr[], int i, int n) {
+        if (i >= n) {
+            return 0;
+        }
+        int max = arr[i];
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+        max = Math.max(max, arr[i] + moon(arr, i + 2, n));
+        max = Math.max(max, moon(arr, i + 1, n));
+        return dp[i] = max;
+    }
+
+    int findMaxSum(int arr[], int n) {
+        dp = new int[100001];
+         Arrays.fill(dp, -1);
+        return moon(arr, 0, n);
+    }
+}
